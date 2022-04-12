d4e09599eb8aef7bf02425f5e45a44ce3c14e645
johnnyracket <john.randolph.harrison@gmail.com>
2022-04-11 21:45:56 -0500

    more styles

--- packages/ui/src/components/Searchbar.tsx
+++ packages/ui/src/components/Searchbar.tsx
@@ -1,6 +1,6 @@
 
 
-import { Button, Grid, Group, Paper, TextInput, Title, TitleOrder, useMantineTheme } from "@mantine/core";
+import { Button, Grid, Group, MultiSelect, Paper, TextInput, Title, TitleOrder, useMantineTheme } from "@mantine/core";
 import { ReactChild } from "react";
 import { Search } from "tabler-icons-react";
 
@@ -8,6 +8,15 @@ type SearchbarProps = {
 
 }
 const Searchbar = () => {
+    const data = [
+        { value: 'react', label: 'React' },
+        { value: 'ng', label: 'Angular' },
+        { value: 'svelte', label: 'Svelte' },
+        { value: 'vue', label: 'Vue' },
+        { value: 'riot', label: 'Riot' },
+        { value: 'next', label: 'Next.js' },
+        { value: 'blitz', label: 'Blitz.js' },
+    ];
 
     const theme = useMantineTheme();
 
@@ -15,14 +24,24 @@ const Searchbar = () => {
         <Paper m="lg" p="lg">
             <Grid>
                 <Grid.Col xs={10}>
-                    <TextInput />
+                    <TextInput placeholder="Search for teammates..." />
                 </Grid.Col>
                 <Grid.Col xs={2} style={{ display: "flex", justifyContent: "center" }}>
                     <Button fullWidth><Search /></Button>
                 </Grid.Col>
-
-
             </Grid>
+            <Group grow>
+                <MultiSelect
+                    data={data}
+                    label="Role"
+                    placeholder="Filter by roles"
+                />
+                <MultiSelect
+                    data={data}
+                    label="Skills"
+                    placeholder="Filter by skills"
+                />
+            </Group>
         </Paper>
     )
 };
